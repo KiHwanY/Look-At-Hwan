81d4d9282990f348f63e549f211234e94fbf4400
KiHwan Yun <drg2524@naver.com>
2024-05-20 23:18:38 +0900

    초기 셋팅 완료

---
+++ src/main/java/com/example/LookAtHwan/LookAtHwanApplication.java
@@ -0,0 +1,13 @@
+package com.example.LookAtHwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookAtHwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookAtHwanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LookAtHwan/LookAtHwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LookAtHwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookAtHwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
