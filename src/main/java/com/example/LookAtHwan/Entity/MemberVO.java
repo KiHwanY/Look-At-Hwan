14e15cb3a6bd7b8c97dec824c10578d5b37e27fc
KiHwan Yun <drg2524@naver.com>
2024-06-03 20:32:37 +0900

    Member Entity Update

--- src/main/java/com/example/LookAtHwan/Entity/MemberVO.java
+++ src/main/java/com/example/LookAtHwan/Entity/MemberVO.java
@@ -18,21 +18,21 @@ public class MemberVO {
     @Column(name = "USER_KEY")
     private Long userKey; // 회원 고유 번호
 
-//    private Long ADMIN_KEY; // 관리자 고유 번호
-//    @Column(length = 30 , name = "USER_ID" )
-//    private String userId; // 회원 아이디
-//    @Column(length = 200 , name = "USER_PW")
-//    private String userPw; // 회원 비밀번호
+    private Long ADMIN_KEY; // 관리자 고유 번호
+    @Column(length = 30 , name = "USER_ID" )
+    private String userId; // 회원 아이디
+    @Column(length = 200 , name = "USER_PW")
+    private String userPw; // 회원 비밀번호
     @Column(length = 30 , name = "USER_NM")
     private String userNm; // 회원 이름
-//    @Column(length = 50, name = "USER_NICKNAME")
-//    private String userNickName; // 회원 닉네임
-//    @Column(length = 30, name = "USER_BIRTH")
-//    private String userBirth; // 생년월일
-//    @Column(length = 10,name = "USER_PHONE")
-//    private String userPhone; // 전화번호
-//    @Column(length = 1 , name = "USER_CK")
-//    private String userCk; // 회원 권한
+    @Column(length = 50, name = "USER_NICKNAME")
+    private String userNickName; // 회원 닉네임
+    @Column(length = 30, name = "USER_BIRTH")
+    private String userBirth; // 생년월일
+    @Column(length = 10,name = "USER_PHONE")
+    private String userPhone; // 전화번호
+    @Column(length = 1 , name = "USER_CK")
+    private String userCk; // 회원 권한
     @Column(length = 6 , name = "ZIPCODE")
     private Integer zipcode; // 우편 번호
     @Column(length = 100 , name ="ADDRESS_BASIC" )
