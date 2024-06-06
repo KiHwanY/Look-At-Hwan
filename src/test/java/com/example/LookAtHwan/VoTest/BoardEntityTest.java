36a5b954cf031072f30930cb9ffcf59170afeb32
KiHwan Yun <drg2524@naver.com>
2024-06-06 16:05:20 +0900

    Board Test Code Init

--- src/test/java/com/example/LookAtHwan/VoTest/BoardEntityTest.java
+++ src/test/java/com/example/LookAtHwan/VoTest/BoardEntityTest.java
@@ -1,8 +1,15 @@
 package com.example.LookAtHwan.VoTest;
 
+import com.example.LookAtHwan.Entity.ProductVO;
+import com.example.LookAtHwan.Entity.ValueObject.Period;
 import com.example.LookAtHwan.Repository.BoardRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class BoardEntityTest {
@@ -10,5 +17,48 @@ public class BoardEntityTest {
     @Autowired
     BoardRepository boardRepository;
 
+//
+//    @Test
+//    public void insertProduct() {
+//        IntStream.rangeClosed(1, 10).forEach(i -> {
+//            Period test = new Period();
+//            test.setCreateDt(LocalDateTime.now());
+//            ProductVO product = ProductVO.builder().proNm("period" + i).period(test).
+//                    build();
+//
+//            productRepository.save(product);
+//        });
+//    }
+//
+//    @Test
+//    public void SelectProductTest(){
+//
+//        Long id = 9L;
+//
+//        Optional<ProductVO> result = productRepository.findById(id);
+//
+//        System.out.println("==================================");
+//
+//        if(result.isPresent()){
+//            ProductVO product = result.get();
+//            System.out.println(product);
+//        }
+//    }
+//    @Test
+//    public void UpdateProductTest(){
+//        Period test = new Period();
+//        test.setUpdateDt(LocalDateTime.now());
+//
+//        ProductVO product = ProductVO.builder().proKey(10L).proNm("ValueObjectUpdateTest").period(test).
+//                build();
+//
+//        productRepository.save(product);
+//    }
+//    @Test
+//    public void DeleteProductTest(){
+//        Long id = 10L;
+//
+//        productRepository.deleteById(id);
+//    }
 
 }
