71884805d7153d6acebd3576193381c667410733
KiHwan Yun <drg2524@naver.com>
2024-06-28 12:40:12 +0900

    test 진행

--- src/main/java/com/example/LookAtHwan/Controller/MemberController.java
+++ src/main/java/com/example/LookAtHwan/Controller/MemberController.java
@@ -1,9 +1,17 @@
 package com.example.LookAtHwan.Controller;
 
+import com.example.LookAtHwan.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+
+    @Autowired
+    MemberService memberService;
+
+
+    
 
 
 }
